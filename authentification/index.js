aadef98ed8b627f1c30672f9754100743e4bbe29
essadany <essadanyya@cy-tech.fr>
2024-03-08 12:40:45 +0100

    working on auth

--- authentification/index.js
+++ authentification/index.js
@@ -1,134 +1,131 @@
-/* Step 1 : session management
-set up a session management using the express-session middleware
-
-add the express-session to your application
-findout how the session is kept between request
-create an api /session that will display the content of the session
-TIPS :
-
-you can use JSON.stringify to display information
-session is available on the server side : req.session
-it could be a good idea to follow the express good practice regarding the session : http://expressjs.com/en/advanced/best-practice-security.html#dont-use-the-default-session-cookie-name */
-
-const express = require('express');
-const session = require('express-session');
+const express = require("express");
 const app = express();
-const path = require('path');
-const port = process.env.PORT || 5001; 
-const redis = require('redis');
-const client = redis.createClient({
-  host: 'localhost',
-  port: 6379
+const port = process.env.PORT || 9000;
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const store = require("store2");
+const jwt = require("jsonwebtoken");
+const redis = require("redis");
+require("dotenv").config();
+const cors = require("cors");
+app.use(cors());
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// create redis client
+let client = redis.createClient({
+  host: "localhost",
+  port: 6380,
+
 });
-client.on('error', (err) => {
-  console.log('Error ' + err);
+client.on("connect", function () {
+  console.log("Connected to Redis...");
 }
 );
-client.on('connect', () => {
-  console.log('Connected to Redis');
+client.on("error", function (err) {
+  console.log("Error " + err);
 }
 );
+client.hmset("test", "firstname", "test", "lastname", "test", "username", "test", "password", "test");
 
-app.use(session({
-  secret: '42',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+// a variable to save a session
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/session', (req, res) => {
-  res.json(req.session);
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "1800s",
+  });
 }
-);
 
-app.get("/test_session", (req, res) => {
-  req.session.username = "test";
-  req.session.save();
-  res.send("session set");
+//Handle the 404 error and change the url with the /autorize route.
+
+app.get("*", (req, res, next) => {
+  if (
+    (req.query.client_id != process.env.CLIENT_ID) &
+    (req.query.scope != "motus_app")
+  ) {
+    console.log("Non authorized to go here");
+    res.status(403).send("Error 403 : non authorized");
+  } else {
+    next();
+  }
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  //show the login page
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
-})
-//authorize
-app.post('/login', (req, res) => {
-  //search for the username in the database
-  client.exists(req.query.username, (err, exists) => {
-    if (err) {
-      console.log(err);
+app.get("/authorize", (req, res, next) => {
+  res.sendFile("/login.html", { root: __dirname + "/public" });
+});
+
+app.post("/authorize", (req, res) => {
+  var check = false;
+  client.hGetAll(req.body.username, (err, user) => {
+    if (user && user.password === req.body.password) {
+      check = true;
+    }
+    if (check) {
+      // Generate a token for the user
+      const access_token = generateAccessToken({ username: req.body.username });
+      console.log(access_token);
+      console.log(req.query.redirect_uri);
+      res.status(302).redirect(req.query.redirect_uri + "?token=" + access_token);
+      res.redirect("http://localhost:3000/");
+      res.send(`Hey there, welcome <a href=\'/logout'>click to logout</a>`);
     } else {
-      if (exists) {
-        //get the password of the username
-        client.hget(req.query.username, 'password', (err, password) => {
-          if (err) {
-            console.log(err);
-          } else {
-            if (password === req.query.password) {
-              // create a session
-              req.session.username = req.query.username;
-              req.session.save();
-              //redirect to the motus game
-              res.redirect('http://localhost:3000');
-            } else {
-              //display an alert
-              console.log('Incorrect password');
-              alert('Incorrect password');
-            }
-          }
-        });
-      } else {
-        //display an alert
-        console.log('Username does not exist');
-        alert('Username does not exist');
-      }
+      res.send("Invalid username or password");
     }
   });
 });
-
-app.get('/register', (req, res) => {
-  //res.json(req.session);
-  res.sendFile(path.join(__dirname, 'public', 'register.html'));
-}
-);
-app.post('/register', (req, res) => {
-  //get the data from the form
-  console.log(req.query);
-  res.redirect('http:localhost:5001');
-  //store the data in redis:
-  client.exists(req.query.username, (err, exists) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (exists) {
-        console.log('Username already exists');
-        alert('Username already exists');
-      } else {
-        client.hmset(req.query.username, ['password', req.query.password, 'firstname', req.query.firstname, 'lastname', req.query.lastname], (err, reply) => {
-          if (err) {
-            console.log(err);
-          }
-          console.log(reply);
-        });
-      }
-    }
-  });
-  
-
-}
-);
- 
-
 
 app.get("/logout", (req, res) => {
   req.session.destroy();
   res.redirect("/");
 });
 
+app.get("/register", (req, res) => {
+  res.sendFile("/register.html", { root: __dirname + "/public" });
+});
+
+app.post("/register", (req, res) => {
+  var check = false;
+  client.hGetAll(req.body.username, (err, user) => {
+    if (user) {
+      check = true;
+    }
+    if (check) {
+      res.status(201).json({
+        erreur: "Il existe déjà cet username",
+      });
+    } else {
+      if (req.body.password != req.body.password2) {
+        res.status(201).json({
+          erreur: "Mauvais mot de passe pour cette session",
+        });
+      } else {
+        client.hmset(req.body.username, {
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          password: req.body.password,
+        });
+        console.log(client.hgetall(req.body.username));
+        var urlconnexion =
+          req.protocol +
+          "://" +
+          req.get("host") +
+          "/authorize?client_id=" +
+          req.query["client_id"] +
+          "&scope=" +
+          req.query["scope"] +
+          "&redirect_uri=" +
+          req.query["redirect_uri"];
+        res.status(201).json({
+          message: "Vous pouvez vous connecter ",
+          Clique_Here: urlconnexion,
+        });
+      }
+    }
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
