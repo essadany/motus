6a4d5cfc2ee66ded81a81272d6f848de9df0b37b
essadany <essadanyya@cy-tech.fr>
2024-03-09 01:03:19 +0100

    final version

--- authentification/index.js
+++ authentification/index.js
@@ -1,91 +1,114 @@
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
-const redis = require('redis');
-let RedisStore = require('connect-redis').default;
-let redisClient = redis.createClient({ socket: {
-  host: 'localhost',
-  port: 6381,
-},
-// Pour la reconnexion automatique
-autoResendUnfulfilledCommands: true,
-autoResubscribe: true });
+const redis = require("redis");
+let RedisStore = require("connect-redis").default;
+let redisClient = redis.createClient({
+  socket: {
+    host: "localhost",
+    port: 6381,
+  },
+  // Pour la reconnexion automatique
+  autoResendUnfulfilledCommands: true,
+  autoResubscribe: true,
+});
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.connect().then(() => {
+  console.log("Connected to Redis");
+});
 const port = 9000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-  store: new RedisStore({ client: redisClient }),
-  secret: 'authentification-redis-secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } 
-}));
-
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: "authentification-redis-secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
 // Configurer les sessions
-app.use(session({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-      maxAge: 3600000 // 1 heure
-  }
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 3600000, // 1 heure
+    },
+  })
+);
 
-
-redisClient.on('error', (err) => {
-  console.log('Erreur Redis :', err);
+redisClient.on("error", (err) => {
+  console.log("Erreur Redis :", err);
 });
 
 // Routes
-app.get('/', (req, res) => {
-  res.sendFile("login.html", { root: './public' });
+app.get("/", (req, res) => {
+  res.sendFile("login.html", { root: "./public" });
 });
 
-app.get('/register', (req, res) => {
-  res.sendFile("register.html", { root: './public' });
+app.get("/register", (req, res) => {
+  res.sendFile("register.html", { root: "./public" });
 });
 
 // Route pour le login
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
   const { username, password } = req.body;
 
   redisClient.hGetAll(username, (err, user) => {
-      if (err) {
-          return res.status(500).send('Erreur de serveur');
-      }
-      if (!user.username || user.password !== password) {
-          return res.status(401).send('Nom d\'utilisateur ou mot de passe incorrect!');
-      }
-      req.session.loggedin = true;
-      req.session.username = username;   
-      res.redirect('http://localhost:3000');
+    if (err) {
+      return res.status(500).send("Erreur de serveur");
+    }
+    if (!user.username || user.password !== password) {
+      return res
+        .status(401)
+        .send("Nom d'utilisateur ou mot de passe incorrect!");
+    }
+    req.session.loggedin = true;
+    req.session.username = username;
+    res.redirect("http://localhost:3000");
   });
 });
 
 // Route pour l'enregistrement
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const { firstname, lastname, username, password } = req.body;
 
-  redisClient.hExists(username, 'username', (err, exists) => {
-      if (err) {
-          return res.status(500).send('Erreur de serveur');
+  redisClient.hExists(username, "username", (err, exists) => {
+    if (err) {
+      return res.status(500).send("Erreur de serveur");
+    }
+    if (exists) {
+      return res.status(400).send("Le nom d'utilisateur est déjà pris.");
+    }
+    redisClient.hSet(
+      username,
+      "firstname",
+      firstname,
+      "lastname",
+      lastname,
+      "username",
+      username,
+      "password",
+      password,
+      (err) => {
+        if (err) {
+          return res
+            .status(500)
+            .send("Erreur lors de l'enregistrement de l'utilisateur");
+        }
+        req.session.loggedin = true;
+        req.session.username = username;
+        res.send("Compte créé avec succès!");
       }
-      if (exists) {
-          return res.status(400).send('Le nom d\'utilisateur est déjà pris.');
-      }
-      redisClient.hSet(username, 'firstname', firstname, 'lastname', lastname, 'username', username, 'password', password, (err) => {
-          if (err) {
-              return res.status(500).send('Erreur lors de l\'enregistrement de l\'utilisateur');
-          }
-          req.session.loggedin = true;
-          req.session.username = username;
-          res.send('Compte créé avec succès!');
-      });
+    );
   });
 });
 
--- motus/index.js
+++ motus/index.js
@@ -1,154 +1,156 @@
-const express = require('express')
-const app = express()
-const fs = require('fs');
-const path = require('path');
-const redis = require('redis');
-let RedisStore = require('connect-redis').default;
-const axios = require('axios');
-const bodyParser = require('body-parser');
-const os = require('os');
-const session = require('express-session');
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+const redis = require("redis");
+let RedisStore = require("connect-redis").default;
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const os = require("os");
+const session = require("express-session");
 const port = process.env.PORT || 3000; // Utilise la variable d'environnement PORT, ou 3000 par défaut
 let score = 0;
 let tries = 0;
-let username = 'useranme';
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
-let SessionClient = redis.createClient({ socket: {
-  host: 'localhost',
-  port: 6381,
-},
-// Pour la reconnexion automatique
-autoResendUnfulfilledCommands: true,
-autoResubscribe: true });
-app.use(session({
-  store: new RedisStore({ client: SessionClient }),
-  secret: 'authentification-redis-secret',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false } 
-}));
-app.post('/logout', (req, res) => {
+let SessionClient = redis.createClient({
+  socket: {
+    host: "localhost",
+    port: 6381,
+  },
+  // Pour la reconnexion automatique
+  autoResendUnfulfilledCommands: true,
+  autoResubscribe: true,
+});
+SessionClient.on("error", (err) => console.log("Redis Client Error", err));
+SessionClient.connect().then(() => {
+  console.log("Connected to Redis");
+});
+app.use(
+  session({
+    store: new RedisStore({ client: SessionClient }),
+    secret: "authentification-redis-secret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+app.post("/logout", (req, res) => {
   req.session.destroy((err) => {
-      if (err) {
-          return console.log(err);
-      }
-      res.redirect('http://localhost:9000');
+    if (err) {
+      return console.log(err);
+    }
+    res.redirect("http://localhost:9000");
   });
 });
 
 //get the score from the score service
-app.post('/getscore', async (req, res) => {
+app.post("/getscore", async (req, res) => {
   const received_data = req.body;
-  const { username, score, tries } = req.body;
+  const { score, tries } = req.body;
   score = score;
   tries = tries;
 });
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  console.log(req.session.username);
-  username = req.session.username;
-})
-const filePath = path.join(__dirname, 'liste_francais_utf8.txt');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+  //console.log(req.session.username);
+  //username = req.session.username;
+});
+const filePath = path.join(__dirname, "liste_francais_utf8.txt");
 var getWordOfTheDay = (filteredWords) => {
-    const date = new Date();
-    const day = date.getDate(); 
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
-    return filteredWords[(day + month + year) % filteredWords.length];
-}
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  return filteredWords[(day + month + year) % filteredWords.length];
+};
 
-app.get('/readfile', (req, res) => {
-  fs.readFile(filePath, 'utf8', (err, data) => {
+app.get("/readfile", (req, res) => {
+  fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
       console.error(err);
-      return res.status(500).send('Erreur lors de la lecture du fichier');
+      return res.status(500).send("Erreur lors de la lecture du fichier");
     }
 
     const words = data.split(/\r?\n|\s/);
 
-    const filteredWords = words.filter(word => word.length > 0);
+    const filteredWords = words.filter((word) => word.length > 0);
 
     res.json(filteredWords);
   });
 });
 
-app.post('/checkword', async (req, res) => {
+app.post("/checkword", async (req, res) => {
   const userWord = req.body.word.toLowerCase(); // Get the word submitted by the user
-  console.log('USERWORD : ', userWord);
+  console.log("USERWORD : ", userWord);
 
-  fs.readFile(filePath, 'utf8', async (err, data) => {
-      if (err) {
-          console.error(err);
-          return res.status(500).send('Erreur lors de la lecture du fichier');
+  fs.readFile(filePath, "utf8", async (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Erreur lors de la lecture du fichier");
+    }
+
+    const words = data.split(/\r?\n|\s/);
+    const filteredWords = words.filter((word) => word.length > 0);
+    const wordOfTheDay = getWordOfTheDay(filteredWords).toLowerCase();
+    console.log(wordOfTheDay);
+
+    let result = [];
+    for (let i = 0; i < userWord.length; i++) {
+      if (userWord[i] === wordOfTheDay[i]) {
+        result.push({ letter: userWord[i], class: "correct" });
+      } else if (wordOfTheDay.includes(userWord[i])) {
+        result.push({ letter: userWord[i], class: "present" });
+      } else {
+        result.push({ letter: userWord[i], class: "absent" });
       }
+    }
 
-      const words = data.split(/\r?\n|\s/);
-      const filteredWords = words.filter(word => word.length > 0);
-      const wordOfTheDay = getWordOfTheDay(filteredWords).toLowerCase();
-      console.log(wordOfTheDay);
+    if (userWord === wordOfTheDay) {
+      score = 1;
+    }
+    console.log("SCORE : ", score);
 
-      let result = [];
-      for (let i = 0; i < userWord.length; i++) {
-          if (userWord[i] === wordOfTheDay[i]) {
-              result.push({ letter: userWord[i], class: 'correct' });
-          } else if (wordOfTheDay.includes(userWord[i])) {
-              result.push({ letter: userWord[i], class: 'present' });
-          } else {
-              result.push({ letter: userWord[i], class: 'absent' });
-          }
-      }
+    // Increment tries
+    tries = 1;
+    console.log("TRIES : ", tries);
 
-      if (userWord === wordOfTheDay) {
-          score = 1;
-      }
-      console.log('SCORE : ', score);
+    // Post score and tries to the score service
+    try {
+      const scoreResponse = await axios.post("http://localhost:5000/setscore", {
+        score: score,
+        tries: tries,
+      });
+      console.log(scoreResponse.data);
 
-      // Increment tries
-      tries=1;
-      console.log('TRIES : ', tries);
+      // Combine both responses
+      const finalResponse = {
+        result: result, // Result of word check
+        scoreData: scoreResponse.data, // Response from score service
+      };
 
-      // Post score and tries to the score service
-      try {
-          const scoreResponse = await axios.post('http://localhost:5000/setscore', {
-              username: os.hostname(),
-              score: score,
-              tries: tries
-          });
-          console.log(scoreResponse.data);
-
-          // Combine both responses
-          const finalResponse = {
-              result: result, // Result of word check
-              scoreData: scoreResponse.data // Response from score service
-          };
-
-          // Send the combined response back to the client
-          res.json(finalResponse);
-          score = 0;
-      } catch (error) {
-          console.error('Error posting to score service:', error);
-          res.status(500).json({ error: "Erreur lors de la communication avec le service de score" });
-      }
+      // Send the combined response back to the client
+      res.json(finalResponse);
+      score = 0;
+    } catch (error) {
+      console.error("Error posting to score service:", error);
+      res.status(500).json({
+        error: "Erreur lors de la communication avec le service de score",
+      });
+    }
   });
 });
 
-
-
-//unix command to kill port 5000 
-//lsof -ti:5000 | xargs kill
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
-app.get('/port', (req, res) => {
-    const user = os.hostname(); // Récupère le nom d'utilisateur
-    res.send(`MOTUS APP working on ${user} port ${port}`);
+app.get("/port", (req, res) => {
+  const user = os.hostname(); // Récupère le nom d'utilisateur
+  res.send(`MOTUS APP working on ${user} port ${port}`);
 });
 // PORT=3000 node index.js # Pour la première instance
 // PORT=4000 node index.js # Pour la deuxième instance
 
-app.get('/health', (req, res) => {
-    res.status(200).send('OK');
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
